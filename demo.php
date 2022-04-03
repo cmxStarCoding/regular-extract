49cb90141c4a8551298563da21adea75bb9947fd
cmx_star <316785038@qq.com>
2022-04-03 10:42:17 +0800

    修改包名

--- demo.php
+++ demo.php
@@ -5,11 +5,8 @@ require 'vendor/autoload.php';
 use Image\Local\Extract\RichTextExtractImageSrc;
 use Image\Local\Exception\RichTextExtractImageException;
 
-
 $content = '<p><img src="https://static001.geekbang.org/resource/image/df/40/df636a4aded0e97727db850d3c406840.png" /><img src="https://static001.geekbang.org/resource/image/df/40/df636a4aded0e97727db850d3c406840.png" /></p>';
-
 $pattern = "/<[img|IMG].*?src=[\'|\"](.*?(?:[\.png|\.jpg]))[\'|\"].*?[\/]?>/";
-
 $host = '192.168.56.56:9501';
 
 try{
@@ -28,7 +25,9 @@ try{
 
 
 }catch (RichTextExtractImageException $richTextExtractImageException){
+    //实际项目需要日志记录异常
 
+    //抛出异常
     throw new RichTextExtractImageException($richTextExtractImageException->getMessage());
 
 }
